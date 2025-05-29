71ac26bc93adc19209ce886886a36b4fc9380c37
rafarush <rgarciagonzalez59@gmail.com>
2025-05-28 23:14:44 -0400

    Period time chooser created and working on License report

--- src/visual/ReportView/ReportView.java
+++ src/visual/ReportView/ReportView.java
@@ -5,6 +5,7 @@ import services.ServicesLocator;
 import visual.DriverView.SelectDriver;
 
 import visual.MainScreen.MainScreen;
+import visual.PeriodTimeChooser;
 import visual.ReportScreen.ReportScreen;
 
 import javax.swing.*;
@@ -354,21 +355,33 @@ public class ReportView extends javax.swing.JPanel {
 
     private void jPanelLicenseMouseClicked(java.awt.event.MouseEvent evt) {
         // TODO add your handling code here:
-        java.sql.Date start = java.sql.Date.valueOf("2020-01-01");
-        java.sql.Date end = java.sql.Date.valueOf("2025-03-31");
-        try {
-            String pdfPath = PDFReportGenerator.createLicenseReportPDF(start, end);
-
-            File file = new File(pdfPath);
-            if (file.exists()) {
-                Desktop.getDesktop().open(file);
-            } else {
-                throw new UnsupportedOperationException("El archivo no existe.");
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                PeriodTimeChooser dialog = new PeriodTimeChooser(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
             }
-        } catch (IOException e) {
-
-            JOptionPane.showMessageDialog(null, "Hubo un problema al crear el reporte.");
-        }
+        });
+//        java.sql.Date start = java.sql.Date.valueOf("2020-01-01");
+//        java.sql.Date end = java.sql.Date.valueOf("2025-03-31");
+//        try {
+//            String pdfPath = PDFReportGenerator.createLicenseReportPDF(start, end);
+//
+//            File file = new File(pdfPath);
+//            if (file.exists()) {
+//                Desktop.getDesktop().open(file);
+//            } else {
+//                throw new UnsupportedOperationException("El archivo no existe.");
+//            }
+//        } catch (IOException e) {
+//
+//            JOptionPane.showMessageDialog(null, "Hubo un problema al crear el reporte.");
+//        }
     }
 
     private void jPanelTestMouseClicked(java.awt.event.MouseEvent evt) {
